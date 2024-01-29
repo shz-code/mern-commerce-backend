b73b40c783aab398de04573a91f91b17a7f8d5fe
shz-code <web.shahidul.alam@gmail.com>
2024-01-29 16:58:01 +0600

    add: Product Model

---
+++ models/product.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+const joi = require("joi");
+
+const productCategory = Schema(
+  {
+    name: String,
+    description: String,
+    price: Number,
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    quantity: Number,
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const validate = (product) => {
+  const schema = joi.object({
+    name: joi.max(255).required(),
+    description: joi.string().max(2000),
+    price: joi.number().required(),
+    quantity: joi.number().required(),
+    category: joi.string().required(),
+  });
+  return schema.validate(category);
+};
+
+module.exports.Product = model("Product", productCategory);
+module.exports.validate = validate;
