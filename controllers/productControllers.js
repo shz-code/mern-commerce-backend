fe369bf487e5c95def21f0327328314205e7de32
shz-code <web.shahidul.alam@gmail.com>
2024-01-29 20:37:04 +0600

    add: Product photo upload

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,19 +1,69 @@
 const _ = require("lodash");
+const { formidable } = require("formidable");
+const fs = require("fs");
 const { Product, validate } = require("../models/product");
 
 module.exports.createProduct = async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  let form = formidable({ keepExtensions: true, uploadDir: `./uploads` });
 
-  const product = new Product(req.body);
-  await product.save();
-  return res.status(201).send({
-    message: "Product created successfully",
-    data: _.pick(product, ["name", "description", "price", "_id", "quantity"]),
+  form.parse(req, (err, fields, files) => {
+    const data = {
+      name: fields.name[0],
+      description: fields.description[0],
+      price: fields.price[0],
+      quantity: fields.quantity[0],
+      category: fields.category[0],
+    };
+    if (err) return res.status(400).send("Something went wrong!!!");
+
+    const { error } = validate(data);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const product = new Product(data);
+
+    // Input field name photo
+    if (files.photo) {
+      fs.readFile(files.photo[0].filepath, async (err, data) => {
+        if (err) return res.status(400).send("Error in image");
+        product.photo.data = data;
+        product.photo.contentType = files.photo[0].mimetype.split("/")[1];
+        try {
+          const result = await product.save();
+          return res.status(201).send({
+            message: "Product created successfully",
+            data: _.pick(result, [
+              "name",
+              "description",
+              "price",
+              "category",
+              "quantity",
+              "_id",
+            ]),
+          });
+        } catch (err) {
+          return res.status(500).send("Internal Server Error!");
+        }
+      });
+    } else {
+      return res.status(400).send("No image provided");
+    }
   });
 };
 
 module.exports.getProducts = async (req, res) => {
   const products = await Product.find().sort({ name: 1 });
   return res.send(products);
+};
+
+module.exports.getProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return res.status(400).send("Product not found");
+  return res.send(product);
+};
+
+module.exports.updateProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return res.status(400).send("Product not found");
+
+  return res.send(product);
 };
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 const {
   createProduct,
   getProducts,
+  getProduct,
+  updateProduct,
 } = require("../controllers/productControllers");
 const authorize = require("../middlewares/authorize");
 const admin = require("../middlewares/admin");
 
 router.route("/").get(getProducts).post([authorize, admin], createProduct);
 
-// router.route("/:id").post(login);
+router.route("/:id").get(getProduct).put([authorize, admin], updateProduct);
 
 module.exports = router;
