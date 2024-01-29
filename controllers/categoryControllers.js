9123f95ea06921491b573fcb7a7440e9dadff64d
shz-code <web.shahidul.alam@gmail.com>
2024-01-29 11:37:19 +0600

    add: Category Controllers

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -1,6 +1,21 @@
 const _ = require("lodash");
 const { Category, validate } = require("../models/category");
 
-module.exports.createCategory = async (req, res) => {};
+module.exports.createCategory = async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-module.exports.getCategories = async (req, res) => {};
+  const category = new Category(_.pick(req.body, ["name"]));
+  await category.save();
+  return res.status(201).send({
+    message: "Category created successfully",
+    data: { name: req.body.name },
+  });
+};
+
+module.exports.getCategories = async (req, res) => {
+  const categories = await Category.find()
+    .sort({ name: 1 })
+    .select({ name: 1 });
+  return res.send(categories);
+};
